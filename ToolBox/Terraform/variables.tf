0f2719b7e5c34f63310d1167cba7277eabe26918
deniskrukk <92173401+deniskrukk@users.noreply.github.com>
2024-12-21 16:43:46 +0200

    Create variables.tf

---
+++ ToolBox/Terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_access_key" {
+  type    = string
+  default = ""
+}
+
+variable "aws_secret_key" {
+  type    = string
+  default = ""
+}
+
+variable "region" {
+  type    = string
+  default = "eu-north-1"
+}
+
+variable "public_ip" {
+  type    = string
+  default = "eipalloc-03ba0d33dc828c5a5"
+}
